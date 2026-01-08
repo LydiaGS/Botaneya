271335bc0901d9b3b0f9c5ab2402952c9f630bcf
amandine letellier <letellier.ama@gmail.com>
2026-01-09 00:38:25 +0100

    first commit

---
+++ javascrpits/accueill.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll('.about-image .slide');
+    let index = 0;
+
+    if (slides.length > 0) {
+        setInterval(() => {
+            slides[index].classList.remove('active');
+            index = (index + 1) % slides.length;
+            slides[index].classList.add('active');
+        }, 2500); 
+    }
+});
