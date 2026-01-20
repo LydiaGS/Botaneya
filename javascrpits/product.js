ccd7e32ef4d7befec05b5c6f7d961e9b742a4e7c
amandine letellier <letellier.ama@gmail.com>
2026-01-20 16:30:32 +0100

    pageproduit

--- javascrpits/product.js
+++ javascrpits/product.js
@@ -111,3 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
     renderCart();
 
 });
+if (cartOverlay) {
+    cartOverlay.classList.add("show");
+    cartOverlay.addEventListener("click", closeCart);
+}
